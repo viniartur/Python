ea1df81ef3f6a66b9f99c084603d838b6fc7001c
viniartur <viniciusarturvieira@hotmail.com>
2024-01-17 17:45:25 -0500

    17-12-2024

---
+++ python/VeryEasy/Convert Age to Days.swift
@@ -0,0 +1,19 @@
+"""
+Convert Age to Days
+Create a function that takes the age in years and returns the age in days.
+
+Examples
+calcAge(65) ➞ 23725
+
+calcAge(0) ➞ 0
+
+calcAge(20) ➞ 7300
+Notes
+Use 365 days as the length of a year for this challenge.
+Ignore leap years and days between last birthday and now.
+Expect only positive integer inputs.
+"""
+
+func calcAge(_ age: Int) -> Int {
+	return age * 365
+}
