605eff87bb1814cace8c4eb491d71789b4671d16
viniartur <viniciusarturvieira@hotmail.com>
2024-01-16 19:31:38 -0500

    python

---
+++ python/VeryEasy/Return the Sum of Two Numbers.swift
@@ -0,0 +1,20 @@
+""" 
+Return the Sum of Two Numbers
+Create a function that takes two numbers as arguments and returns their sum.
+
+Examples
+addition(3, 2) ➞ 5
+
+addition(-3, -6) ➞ -9
+
+addition(7, 3) ➞ 10
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+https://edabit.com/challenge/C6pdpgxDQFh7KpX9j
+"""
+func addition(_ a: Int, _ b: Int) -> Int {
+	return a + b
+}
